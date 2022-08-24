4d602dab7cd05cea2dd1d81de7d12a53094a2129
Moe Jangda <moe@squareup.com>
2022-08-24 12:10:37 -0500

    implement core
    Co-authored-by: Rebecca Corcillo <rcorcillo@squareup.com>

--- protocols-prototype/protocol.js
+++ protocols-prototype/protocol.js
@@ -1,3 +1,8 @@
+// message method -> protocol `allow.to` action map
+const ACTION_MAP = {
+  'CollectionsWrite': 'create'
+}
+
 class Protocol {
   constructor(protocolDefinition) {
     // assumed that protocolDefinition is valid
@@ -20,6 +25,31 @@ class Protocol {
    */
   getLabel(schema) {
     return this.schemaMap[schema];
+  }
+
+  getRuleset(labelPath = []) {
+    let { structure } = this.protocolDefinition;
+    let ruleset;
+
+    // just here for debugging for now
+    let traversed = [];
+
+    for (let label of labelPath) {
+      traversed.push(label);
+      ruleset = structure[label];
+
+      if (!ruleset) {
+        throw new Error(`ruleset for ${traversed.join('.')} not found in protocol`);
+      }
+
+      structure = ruleset.records;
+    }
+
+    return ruleset;
+  }
+
+  getActionForMethod(method) {
+    return ACTION_MAP[method];
   }
 }
 
--- protocols-prototype/test.js
+++ protocols-prototype/test.js
@@ -6,27 +6,14 @@ const protocolDefs = {
   'tbdex': protocolDefinition
 }
 
-const ask = {
-  contextId: 1,
-  objectId: 1,
-  protocol: 'tbdex',
-  schema: 'https://tbdex.org/protocol/ask',
-  target: 'bob'
-};
-
-const conditionalOffer = {
-  contextId: 1,
-  objectId: 1,
-  parentId: 1,
-  protocol: 'tbdex',
-  schema: 'https://tbdex.org/protocol/conditional-offer',
-  target: 'alice'
-};
+// in-memory message store. key/value: objectId -> message
+const messageStore = {};
 
 /**
  * 
  * @param {Object} message
  * @param {Number} message.contextId
+ * @param {String} message.method
  * @param {Number} message.objectId
  * @param {Number} message.parentId
  * @param {String} message.protocol
@@ -53,7 +40,85 @@ function processMessage(message) {
       throw new Error(`${message.schema} schema not allowed in ${message.protocol} protocol`);
     }
 
+    let { parentId } = message;
+    const labelPath = [schemaLabel];
 
+    // TODO: build JSONPath adherent graph as parents are traversed so that `allow` rules can be
+    // evaluated
+    while (parentId) {
+      //! IO
+      const parent = messageStore[message.objectId];
+      if (!parent) {
+        throw new Error('parent not found');
+      }
 
+      if (message.protocol !== parent.protocol) {
+        throw new Error('parent protocol must match');
+      }
+
+      const parentLabel = protocol.getLabel(parent.schema);
+      labelPath.push(parentLabel);
+    }
+
+    // TODO: need to capture properties set at the root of the structure
+    // e.g. `contextedRequired` and `protectedRuleset` are only defined at
+    // the root
+    const messageRuleset = protocol.getRuleset(labelPath);
+
+    // naively default contextRequired to true if undefined in ruleset
+    const { contextRequired = true } = messageRuleset;
+
+    if (contextRequired && !message.contextId) {
+      throw new Error('message must contain contextId');
+    }
+
+    // evaluate `allow`
+    // TODO: figure out whether `allow` MUST always be present. assuming it is for now
+    const { allow } = messageRuleset;
+    let isAllowed = false;
+    let allowConditions;
+
+    if (allow.anyone) {
+      isAllowed = true;
+      allowConditions = allow.anyone;
+    }
+
+    if (!isAllowed) {
+      throw new Error("not allowed.")
+    }
+
+    // TODO: add eval for `allow.participants`
+    // TODO: add eval for `allow.recipients`
+
+    const action = protocol.getActionForMethod(message.method);
+
+    if (!allowConditions.to.includes(action)) {
+      throw new Error(`${message.method} not allowed`);
+    }
+
+    // TODO: add eval for `allowConditions.of`
+
+    messageStore[message.objectId] = message;
   }
 }
+
+const ask = {
+  contextId: 1,
+  method: 'CollectionsWrite',
+  objectId: 1,
+  protocol: 'tbdex',
+  schema: 'https://tbdex.org/protocol/ask',
+  target: 'bob'
+};
+
+processMessage(ask);
+console.log(JSON.stringify(messageStore, null, 2));
+
+const conditionalOffer = {
+  contextId: 1,
+  objectId: 1,
+  parentId: 1,
+  protocol: 'tbdex',
+  schema: 'https://tbdex.org/protocol/conditional-offer',
+  target: 'alice'
+};
